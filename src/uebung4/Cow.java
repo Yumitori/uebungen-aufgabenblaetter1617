5c6365a5eeec5191ab57601cc9c2fa17f9940b71
u434740 <u434740@R-006.cadlab.tu-berlin.de>
2016-11-15 17:46:48 +0100

    tsv

--- src/uebung4/Cow.java
+++ src/uebung4/Cow.java
@@ -3,13 +3,27 @@ package uebung4;
 class Cow {
 
 	private String name;
-
+	private boolean hungry = true;
+	private static int cowCounter = 0;
+	
+	
 	Cow (String name) {
 		this.name = name;
+		cowCounter++;
 	}
 
 	String getName() {
 		return this.name;
+	}
+	static int getcowCounter () {
+		return cowCounter;
+	}
+	public void toggleHungry() {
+		hungry =! hungry;
+	}
+
+	public boolean isHungry() {
+		return hungry;
 	}
 	
 }
--- src/uebung4/Farm.java
+++ src/uebung4/Farm.java
@@ -3,13 +3,22 @@ package uebung4;
 class Farm {
 
 	public static void main(String[] args) {
+		
 		Cow cow1 = new Cow("Berta");
 		Cow cow2 = new Cow("Hilde");
 		Cow cow3 = new Cow("Miltank");
+		Cow cow4 = new Cow("Hoeneﬂ");
 			
 		System.out.println( cow1.getName() ); 
 		System.out.println( cow2.getName() );
 		System.out.println( cow3.getName() );
+		
+		System.out.println("Anzahl: " + Cow.getcowCounter() );
+		
+		Farmer farmer = new Farmer();
+		System.out.println("Kuh hungrig: "+ cow1.isHungry());
+		farmer.feedCow(cow1);
+		System.out.println("Kuh hungrig: "+ cow1.isHungry());
 	}
 
 }
